6fe4afdfd92424bbdf970a4964fcc3ba7792a3f2
KuraoHikari <dewaindra705@gmail.com>
2022-11-10 16:21:55 +0800

    update golang

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package gorou_tine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -77,5 +78,70 @@ func TestBufferedChannel(t *testing.T){
 }
 
 func TestRangeChannel(t *testing.T){
-	
+	channel := make(chan string, 3)
+	//defer close(channel)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data ", data)
+	}
+	fmt.Println("selesai")
+}
+func TestSelectChannel(t *testing.T){
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for{
+		select {
+			case data := <-channel1:
+				fmt.Println("Data dari Channel 1", data)
+				counter++
+			case data := <-channel2:
+				fmt.Println("Data dari Channel 2", data)
+				counter++
+			
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+func TestSelectChannelDefault(t *testing.T){
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for{
+		select {
+			case data := <-channel1:
+				fmt.Println("Data dari Channel 1", data)
+				counter++
+			case data := <-channel2:
+				fmt.Println("Data dari Channel 2", data)
+				counter++
+			default: 
+				fmt.Println("Tunggu datanya ya jancok")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
